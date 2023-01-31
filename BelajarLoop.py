0febb383da8db477df63cc65a335251316ec94ce
farhanhibatullah <89978615+farhanhibatullah@users.noreply.github.com>
2023-01-31 12:08:51 +0700

    perubahan pertama

--- BelajarLoop.py
+++ BelajarLoop.py
@@ -1,11 +1,15 @@
+#while loop
 n = 10
 while n > 0:
     print(n)
     n = n - 1
 print("mulai")
 
-n = 10
-for x in n:
-    print(n)
-    n = n - 1
-print("mulai")
+
+#for loop
+n = [1,1,1,1,2,1,1,1]
+indeks = [0,1,2,3,4,5,6,7]
+
+for x in indeks:
+    print("indeks ke-", indeks[x], "angkanya adalah ", n[x])
+print("jumlah angka 1 di dalam list adalah ", n.count(1))
