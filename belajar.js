71d94af88c10aa85a424df34c2d0d41318eb91a2
farhanhibatullah <89978615+farhanhibatullah@users.noreply.github.com>
2023-02-01 12:05:29 +0700

    verify tensorflow.js

--- belajar.js
+++ belajar.js
@@ -1,3 +1,6 @@
 const data = {nama: "Farhan", pekerjaan: "Mahasiswa"};
 data.umur = 30;
 console.log(data);
+
+const tf = require('@tensorflow/tfjs');
+console.log('TensorFlow.js version:', tf.version.tfjs);
